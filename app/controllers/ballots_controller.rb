b517f06b0e7e9331a1d694562048589104e12821
Gillian Wenhold <gwenhold@mac.com>
2019-09-12 13:14:26 -0400

    Created new landing page, moved vote counter part to next page

--- app/controllers/ballots_controller.rb
+++ app/controllers/ballots_controller.rb
@@ -1,5 +1,5 @@
 class BallotsController < ApplicationController
-  before_action :set_ballot, only: [:show, :update]
+  before_action :set_ballot, only: [:show]
 
   def index
     @ballots =
@@ -15,16 +15,7 @@ class BallotsController < ApplicationController
     render json: @ballot.to_json(only: %i[id first_choice second_choice third_choice], include: [round: { only: %i[round_number] }])
   end
 
-  def create
-    ballot = Ballot.create(ballot_params)
-    render json: ballot, status: 201
-  end
-
 private
-
-  def ballot_params
-    params.permit(:first_choice, :second_choice, :third_choice)
-  end
 
   def set_ballot
     @ballot = Ballot.find(params[:id])
--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,3 +1,4 @@
 class BaseController < ApplicationController
   def index; end
+  def show; end
 end
