4e32c54103047709fe0f9c0f0c94f5f2ff13f071
Matt Crum <mattscottcrum@gmail.com>
2019-10-06 12:41:49 -0500

    postgres migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_10_164102) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ballots", force: :cascade do |t|
     t.integer "first_choice"
     t.integer "second_choice"
